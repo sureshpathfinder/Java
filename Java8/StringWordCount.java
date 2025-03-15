2826dca31eadc1db257233968b0adb5b0a79e028
SureshBalaraman <57792087+sureshpathfinder@users.noreply.github.com>
2025-03-15 19:55:36 +0530

    Update and rename StringWordCound to StringWordCount.java

--- Java8/StringWordCount.java
+++ Java8/StringWordCount.java
@@ -5,7 +5,7 @@ import java.util.function.Function;
 import java.util.stream.*;
 
 // The main method must be in a class named "Main".
-class WordCount {
+class StringWordCount {
     public static void main(String[] args) {
         String s1 = "india wins a news from india states india wins";
         Map<String, Long> wordCount = Arrays.stream(s1.split(" "))
